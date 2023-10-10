6151c1e0f47103ca21515e98083e53ffb524810c
fransfilastap@live.com <fransfilastap@Live.com>
2023-10-10 13:58:02 +0700

    font

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,12 @@ function App() {
       <div className="relative h-full flex flex-col justify-center items-center overflow-hidden w-full">
         <div className="z-30 flex flex-col justify-center items-center gap-2">
           <LogoLDCC />
-          <h1 className="text-white text-2xl lg:text-6xl font-extrabold ">
+          <h1 className="text-white text-2xl lg:text-6xl font-roboto">
             LDCC Awards 2023
           </h1>
-          <div className="p-4 w-full border-4  min-h-[10vh] lg:min-h-[30vh] flex justify-center items-center shadow-lg">
+          <div className="p-4 w-full border-4 min-h-[10vh] lg:min-h-[30vh] flex justify-center items-center shadow-lg">
             <Countdown
-              className="text-white text-5xl lg:text-8xl text-center"
+              className="text-white font-roboto text-5xl lg:text-8xl text-center"
               date={voteCloseTime}
             >
               <Completionist />
@@ -34,7 +34,7 @@ function App() {
         >
           <source
             src={
-              "https://res.cloudinary.com/dhtk5fgay/video/upload/v1695274328/pexels-rostislav-uzunov-8303104_1080p_ujbmdt.mp4"
+              "https://res.cloudinary.com/dhtk5fgay/video/upload/v1690026504/pexels_videos_2759484_720p_nkk9pg.mp4"
             }
             type="video/mp4"
           />

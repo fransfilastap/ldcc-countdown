d3630f3ff717d99534fcb766e214a09e97c142d4
fransfilastap@live.com <fransfilastap@Live.com>
2023-10-12 12:46:28 +0700

    completionist

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Countdown from "react-countdown";
 import LogoLDCC from "./logo";
 
 const Completionist = () => (
-  <span className="text-white text-3xl">Vote Closed.</span>
+  <span className="text-3xl text-white">Voting selesai. ✨</span>
 );
 
 function App() {
@@ -10,15 +10,15 @@ function App() {
 
   return (
     <main className="bg-black h-[100vh] w-full flex flex-col items-center justify-center">
-      <div className="relative h-full flex flex-col justify-center items-center overflow-hidden w-full">
-        <div className="z-30 flex flex-col justify-center items-center gap-2">
+      <div className="relative flex flex-col items-center justify-center w-full h-full overflow-hidden">
+        <div className="z-30 flex flex-col items-center justify-center gap-2">
           <LogoLDCC />
-          <h1 className="text-white text-2xl lg:text-6xl font-roboto">
+          <h1 className="text-2xl text-white lg:text-6xl font-roboto">
             LDCC Awards 2023
           </h1>
           <div className="p-4 w-full border-4 min-h-[10vh] lg:min-h-[30vh] flex justify-center items-center shadow-lg">
             <Countdown
-              className="text-white font-roboto text-5xl lg:text-8xl text-center"
+              className="text-5xl text-center text-white font-roboto lg:text-8xl"
               date={voteCloseTime}
             >
               <Completionist />
